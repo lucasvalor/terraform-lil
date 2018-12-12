f772430aa5f3730178113ab3b478ec73caef4a3f
Lucas Valor <lucasvalor@gmail.com>
2018-12-11 22:39:57 -0300

    cuarto commit

--- resources.tf
+++ resources.tf
@@ -1,9 +1,9 @@
 resource "google_compute_network" "our_development_network" {
     name = "devnetwork"
-    auto_create_subnetworks = true
+    auto_create_subnetworks = false
 }
 
-resource "aws_vpc" "enviroment-example-two" {
+resource "aws_vpc" "environment-example-two" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support = true
@@ -15,4 +15,5 @@ resource "aws_vpc" "enviroment-example-two" {
 resource "aws_subnet" "subnet1" {
   cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
   vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "us-west-2a"
 }
